f529b75190b231070e391c792503a5bcb101b462
Md Tarek Rahman <mdtarekrahman209@gmail.com>
2026-05-20 17:09:25 +0600

    initial setup

---
+++ fetchData.py
@@ -0,0 +1,19 @@
+import requests
+import pandas as pd
+
+def get_weather_data():
+    url = "https://archive-api.open-meteo.com/v1/archive"
+
+    params = {
+        "latitude": 23.8103,   # Dhaka
+        "longitude": 90.4125,
+        "start_date": "2026-04-01",
+        "end_date": "2026-04-30",
+        "hourly": "temperature_2m,precipitation,wind_speed_10m"
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    df = pd.DataFrame(data['hourly'])
+    return df
