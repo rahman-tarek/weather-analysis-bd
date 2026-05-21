b17daa8d35bb4eb388913af59061262e4b81ab1b
Md Tarek Rahman <mdtarekrahman209@gmail.com>
2026-05-21 15:38:03 +0600

    average temperature in april

--- analyze_weather.py
+++ analyze_weather.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
 def analyze_weather(df):
+
+    # Total rain 
     total_rain = df['precipitation'].sum()
-    return total_rain
+    # Average temperature
+    avg_temp = df['temperature_2m'].mean()
+    return total_rain, avg_temp
--- main.py
+++ main.py
@@ -6,7 +6,10 @@ def main():
     weather_df = get_weather_data()
 
     # Analyze weather data
-    total_rain = analyze_weather(weather_df)
-    print(f"Total rain in April 2026: {total_rain} mm")
+    total_rain, avg_temp = analyze_weather(weather_df)
+
+    print("Weather Analysis for Dhaka - April 2026")
+    print(f"Total Rain in April 2026: {total_rain} mm")
+    print(f"Average Temperature in April 2026: {avg_temp}°C")
 
 main()
