50ef794055a99fd67acdfd9ee5effbdbd133d2b6
Md Tarek Rahman <mdtarekrahman209@gmail.com>
2026-05-22 22:57:02 +0600

    coolest day in april

--- analyze_weather.py
+++ analyze_weather.py
@@ -8,9 +8,11 @@ def analyze_weather(df):
     avg_temp = df['temperature_2m'].mean()
     # Hottest day
     hottest_day = df.loc[df.temperature_2m.idxmax()]['time']
+    # Coolest day
+    coolest_day = df.loc[df.temperature_2m.idxmin()]['time']
     # Max temperature
     mx_temp = df['temperature_2m'].max()
     # Min temperature
     mn_temp = df['temperature_2m'].min()
 
-    return total_rain, avg_temp, hottest_day, mx_temp, mn_temp
+    return total_rain, avg_temp, hottest_day, mx_temp, mn_temp, coolest_day
--- main.py
+++ main.py
@@ -6,13 +6,13 @@ def main():
     weather_df = get_weather_data()
 
     # Analyze weather data
-    total_rain, avg_temp, hottest_day, mx_temp, mn_temp = analyze_weather(weather_df)
+    total_rain, avg_temp, hottest_day, mx_temp, mn_temp, coolest_day = analyze_weather(weather_df)
 
 
     print("Weather Analysis for Dhaka - April 2026")
     print(f"Total Rain in April 2026: {total_rain} mm")
     print(f"Average Temperature in April 2026: {avg_temp}°C")
     print(f"Hottest Day in April 2026: {hottest_day}, Temperature: {mx_temp}°C")
-    print(f"Minimum Temperature in April: {mn_temp}")
+    print(f"Coolest Day in April 2026: {coolest_day}, Temperature: {mn_temp}°C")
 
 main()
